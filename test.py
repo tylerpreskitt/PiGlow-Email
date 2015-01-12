f772e2fd8476aa4264a35181f7878fea332b1487
tylerpreskitt <tylerpreskitt@gmail.com>
2015-01-11 22:11:13 -0500

    Update test.py

--- test.py
+++ test.py
@@ -11,6 +11,6 @@ ledbrightness = 20
 ###resets lights to off
 piglow.all(0)
 
-newemails = int(feedparser.parse("https://" + USERNAME + ":" + PASSWORD + "@mail.google.com/gmail/feed/atom")["feed"]["fullcount"])
+newmails = int(feedparser.parse("https://" + USERNAME + ":" + PASSWORD + "@mail.google.com/gmail/feed/atom")["feed"]["fullcount"])
 if newmails > 0:
   piglow.all(ledbrightness)
